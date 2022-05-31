e8ca07edf8c0b9daf8ab037f585b70cb0f349296
Wrafailano <rafailanow@gmail.com>
2022-05-31 19:54:40 -0400

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -4,7 +4,8 @@ public class gitAndGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("today we learn gitHub");
+		System.out.print("today is a very good day");
 	}
 
 }
